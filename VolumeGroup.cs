52660af119a88f407e4cadb3f3c6aef36cb1b292
Alex Guo <alexand.guo@nutanix.com>
2018-02-27 22:34:09 -0500

    Implement Get-VolumeGroup

---
+++ VolumeGroup.cs
@@ -0,0 +1,69 @@
+using System.Management.Automation;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Nutanix {
+
+public class VolumeGroup {
+  public string Name;
+  public string Id;
+
+  // 'Uid' is VMware's equivalent field for Nutanix's Uuid.
+  public string Uid;
+  public string Uuid;
+  public dynamic json;
+
+  // TODO Mtu, NumPorts, ExtensionData, NumPortsAvailable, Key, Nic, VMHostId,
+  // VMHost, VMHostUid, Nic
+
+  public VolumeGroup(dynamic json) {
+    // Special property 'json' stores the original json.
+    this.json = json;
+    this.json.Property("status").Remove();
+    this.json.api_version = "3.1";
+
+    Name = json.spec.name;
+    Id = json.spec.resources.vlan_id;
+    Uuid = json.metadata.uuid;
+    Uid = Uuid;
+  }
+}
+
+[CmdletAttribute(VerbsCommon.Get, "VolumeGroup")]
+public class GetVolumeGroupCmdlet : Cmdlet {
+  [Parameter()]
+  public string Uuid { get; set; } = "";
+
+  protected override void ProcessRecord() {
+    if (!String.IsNullOrEmpty(Uuid)) {
+      WriteObject(GetVolumeGroupByUuid(Uuid));
+      return;
+    }
+
+    WriteObject(GetAllVolumeGroups());
+  }
+
+  public static VolumeGroup GetVolumeGroupByUuid(string uuid) {
+    // TODO: validate using UUID regexes that 'uuid' is in correct format.
+    var json = Util.RestCall("/volume_groups/" + uuid, "GET", "" /* requestBody */);
+    return new VolumeGroup(json);
+  }
+
+  public static VolumeGroup[] GetAllVolumeGroups() {
+    var json = Util.RestCall("/volume_groups/list", "POST", "{}");
+    if (json.entities.Count == 0) {
+      return new VolumeGroup[0];
+    }
+    VolumeGroup[] volume_groups = new VolumeGroup[json.entities.Count];
+    for (int i = 0; i < json.entities.Count; ++i) {
+      volume_groups[i] = new VolumeGroup(json.entities[i]);
+    }
+    return volume_groups;
+  }
+}
+
+}
+
