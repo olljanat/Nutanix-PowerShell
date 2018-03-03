ca6d339b2e38c7a911e75ba51376d02202baa97b
Alex Guo <alexand.guo@nutanix.com>
2018-03-02 21:56:58 -0500

    test/image.ps1: add testcase for creating images

--- test/image.ps1
+++ test/image.ps1
@@ -4,7 +4,7 @@
 
 # Assume running from $TOP, so the DLL will be in build dir.
 Import-Module ./build/NtnxPoshSDK.dll -force
-$ClusterIp = "10.4.48.97"
+$ClusterIp = "10.4.50.1"
 $Password = ConvertTo-SecureString 'Nutanix.123' -AsPlainText -Force
 New-NTNX -Server $ClusterIp -UserName admin -Password $Password `
   -AcceptInvalidSslCerts
@@ -24,3 +24,10 @@ Get-Image -Uuid 'ddf96624-438a-4d9c-94ec-823570224f69'
 #------------------------------------------------------------------------------
 
 Remove-Image -Uuid 'ddf96624-438a-4d9c-94ec-823570224f69'
+
+#------------------------------------------------------------------------------
+# POST paths.
+#------------------------------------------------------------------------------
+
+$image_url = 'http://earth.corp.nutanix.com/acro_images/automation/ahv_guest_os/DSK/centos73_vdisk.raw'
+New-Image -Name centos73 -URL $image_url -Description 'FROM SDK'
