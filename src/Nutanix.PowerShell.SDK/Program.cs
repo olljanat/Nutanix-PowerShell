f714f66276dacf61cbbe8e0ab95d35c58cb86860
JonKohler <jon@nutanix.com>
2018-06-12 12:34:21 -0400

    adding dotnet stuff

---
+++ src/Nutanix.PowerShell.SDK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nutanix.PowerShell.SDK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
