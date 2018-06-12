33feea69ac55a8ac22f013b5f4a55d67b4e7ebc2
JonKohler <jon@nutanix.com>
2018-06-12 16:33:21 -0400

    fixes

--- src/Nutanix.PowerShell.SDK/Task.cs
+++ src/Nutanix.PowerShell.SDK/Task.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Management.Automation;
 
-namespace Nutanix
-{
-  public class Task
-  {
+namespace Nutanix {
+  public class Task {
     public string Uuid { get; set; } = string.Empty;
     public string Status { get; set; } = string.Empty;
     public string StartTime { get; set; } = string.Empty;
@@ -16,8 +14,7 @@ namespace Nutanix
     public int DefaultPollTimeoutSecs { get; set; } = 2147483;
     public int DefaultPollIntervalMs = 500;
     public dynamic json { get; set; }
-    public Task(dynamic json)
-    {
+    public Task(dynamic json) {
       Uuid = json.uuid;
       Status = json.status;
       StartTime = json.start_time;
@@ -29,30 +26,24 @@ namespace Nutanix
       this.json = json;
     }
 
-    public Task(string uuid)
-    {
+    public Task(string uuid) {
       Uuid = uuid;
     }
 
-    public static Task FromUuidInJson(dynamic json)
-    {
+    public static Task FromUuidInJson(dynamic json) {
       return new Task(json.status.execution_context.task_uuid.ToString());
     }
 
-    public Task Wait()
-    {
+    public Task Wait() {
       return Wait(DefaultPollTimeoutSecs);
     }
 
-    public Task Wait(int timeoutSecs)
-    {
+    public Task Wait(int timeoutSecs) {
       DateTime start = DateTime.Now;
-      while ((DateTime.Now - start).TotalMilliseconds < timeoutSecs * 1000)
-      {
+      while ((DateTime.Now - start).TotalMilliseconds < timeoutSecs * 1000) {
         System.Threading.Thread.Sleep(DefaultPollIntervalMs);
         var task = GetTaskCmdlet.GetTaskByUuid(Uuid);
-        if (task.Status != "RUNNING")
-        {
+        if (task.Status != "RUNNING") {
           return task;
         }
       }
@@ -61,24 +52,20 @@ namespace Nutanix
   }
 
   [CmdletAttribute(VerbsCommon.Get, "Task")]
-  public class GetTaskCmdlet : Cmdlet
-  {
+  public class GetTaskCmdlet : Cmdlet {
     [Parameter]
     public string Uuid { get; set; } = string.Empty;
 
     [Parameter]
     public Task Task { get; set; }
 
-    protected override void ProcessRecord()
-    {
-      if (!string.IsNullOrEmpty(Uuid))
-      {
+    protected override void ProcessRecord() {
+      if (!string.IsNullOrEmpty(Uuid)) {
         WriteObject(GetTaskByUuid(Uuid));
         return;
       }
 
-      if (Task != null)
-      {
+      if (Task != null) {
         WriteObject(GetTaskByUuid(Task.Uuid));
         return;
       }
@@ -86,8 +73,7 @@ namespace Nutanix
       throw new Exception("Expected either -Uuid or -Task");
     }
 
-    public static Task GetTaskByUuid(string uuid)
-    {
+    public static Task GetTaskByUuid(string uuid) {
       // TODO: validate using UUID regexes that 'uuid' is in correct format.
       var json = Util.RestCall("/tasks/" + uuid, "GET", string.Empty /* requestBody */ );
       return new Task(json);
