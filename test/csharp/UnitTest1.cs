b8dcf2b47f2c47c40d73678faf8a83c588a04c23
Alex Guo <alexand.guo@nutanix.com>
2018-06-13 09:07:21 -0400

    add header comment to UnitTest1.cs

--- test/csharp/UnitTest1.cs
+++ test/csharp/UnitTest1.cs
@@ -1,3 +1,11 @@
+// Purpose: VM (Virtual Machines) source for 'Nutanix.PowerShell.SDK'
+// Author: Nutanix
+// Copyright: Nutanix, 2018
+// Owner: PowerShell@nutanix.com
+// Maintainer(s):
+//   Jon Kohler  (Nutanix, JonKohler)
+//   Alex Guo    (Nutanix, mallochine)
+
 using System;
 using Xunit;
 
