1601d9404733f47413d070af8c4198c83ed9914f
Alex Guo <alexand.guo@nutanix.com>
2018-06-13 08:58:44 -0400

    Fix Program.cs lint issues

--- src/Nutanix.PowerShell.SDK/ExceptionClasses.cs
+++ src/Nutanix.PowerShell.SDK/ExceptionClasses.cs
@@ -10,9 +10,11 @@ using System;
 
 public class NtnxException : Exception
 {
-  public NtnxException() : base() {
+  public NtnxException() : base()
+  {
   }
 
-  public NtnxException(string message) : base(message) {
+  public NtnxException(string message) : base(message)
+  {
   }
 }
--- src/Nutanix.PowerShell.SDK/Program.cs
+++ src/Nutanix.PowerShell.SDK/Program.cs
@@ -1,10 +1,18 @@
+// Purpose: Exception classes source for 'Nutanix.PowerShell.SDK'
+// Author: Nutanix
+// Copyright: Nutanix, 2018
+// Owner: PowerShell@nutanix.com
+// Maintainer(s):
+//   Jon Kohler  (Nutanix, JonKohler)
+//   Alex Guo    (Nutanix, mallochine)
+
 using System;
 
 namespace Nutanix.PowerShell.SDK
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
