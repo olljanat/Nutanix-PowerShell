a0e20f4e757a1341de9dc9dae5f745b0c8395398
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2019-01-02 12:41:35 -0600

    Add getimages_all function

--- private/api/Nutanix/Powershell/NutanixIntentfulAPI.cs
+++ private/api/Nutanix/Powershell/NutanixIntentfulAPI.cs
@@ -1801,6 +1801,97 @@ namespace Nutanix.Powershell
             }
         }
 
+        public async System.Threading.Tasks.Task GetImages_All(Nutanix.Powershell.Models.IVmListMetadata body, System.Func<System.Net.Http.HttpResponseMessage, System.Threading.Tasks.Task<Nutanix.Powershell.Models.IImageIntentResource[]>, System.Threading.Tasks.Task> onOK, System.Func<System.Net.Http.HttpResponseMessage, System.Threading.Tasks.Task<Nutanix.Powershell.Models.IVmStatus>, System.Threading.Tasks.Task> onDefault, Microsoft.Rest.ClientRuntime.IEventListener eventListener, Microsoft.Rest.ClientRuntime.ISendAsync sender, Nutanix.Powershell.Models.NutanixCredential credential)
+        {
+
+            // set the body to fetch the first
+            // Constant Parameters
+            using( NoSynchronizationContext )
+            {
+                // construct URL
+                var _url = new System.Uri((
+                        $"{credential.Uri.ToString()}/api/nutanix/v3//images/list"
+                        ).TrimEnd('?','&'));
+
+                var hasNext = true;
+
+                var _itemsPerPage = 100;
+                var _totalItems = 0;
+                var _offset = 0;
+
+                body.Length = _itemsPerPage;
+                body.Offset = _offset;
+
+                List<Nutanix.Powershell.Models.IImageIntentResource> result = new List<Nutanix.Powershell.Models.IImageIntentResource>();
+                while (hasNext) {
+
+                    await eventListener.Signal(Microsoft.Rest.ClientRuntime.Events.URLCreated, _url); if( eventListener.Token.IsCancellationRequested ) { return; }
+
+                    // generate request object
+                    var request =  new System.Net.Http.HttpRequestMessage(Microsoft.Rest.ClientRuntime.Method.Post, _url);
+                    await eventListener.Signal(Microsoft.Rest.ClientRuntime.Events.RequestCreated, _url); if( eventListener.Token.IsCancellationRequested ) { return; }
+
+                    await eventListener.Signal(Microsoft.Rest.ClientRuntime.Events.HeaderParametersAdded, _url); if( eventListener.Token.IsCancellationRequested ) { return; }
+                    // set body content
+                    request.Content = new System.Net.Http.StringContent(null != body ? body.ToJson(null).ToString() : @"{}", System.Text.Encoding.UTF8);
+                    request.Content.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/json");
+
+                    var byteArray = System.Text.Encoding.ASCII.GetBytes($"{credential.Username}:{CreateString(credential.Password)}");
+                    request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", System.Convert.ToBase64String(byteArray));
+                    await eventListener.Signal(Microsoft.Rest.ClientRuntime.Events.BodyContentSet, _url); if( eventListener.Token.IsCancellationRequested ) { return; }
+
+                    // make the call
+                    System.Net.Http.HttpResponseMessage _response = null;
+                    try
+                    {
+                        await eventListener.Signal(Microsoft.Rest.ClientRuntime.Events.BeforeCall, request); if( eventListener.Token.IsCancellationRequested ) { return; }
+                        _response = await sender.SendAsync(request, eventListener);
+                        await eventListener.Signal(Microsoft.Rest.ClientRuntime.Events.ResponseCreated, _response); if( eventListener.Token.IsCancellationRequested ) { return; }
+                        var _contentType = _response.Content.Headers.ContentType?.MediaType;
+
+                        switch ( _response.StatusCode )
+                        {
+                            case System.Net.HttpStatusCode.OK:
+                            {
+                                await eventListener.Signal(Microsoft.Rest.ClientRuntime.Events.BeforeResponseDispatch, _response); if( eventListener.Token.IsCancellationRequested ) { return; }
+                                var f = await _response.Content.ReadAsStringAsync().ContinueWith( respBody => Nutanix.Powershell.Models.ImageListIntentResponse.FromJson(Carbon.Json.JsonNode.Parse(respBody.Result)) );
+                                result.AddRange(f.Entities);
+                                _totalItems = f.Metadata.TotalMatches.Value;
+
+                                _offset += _itemsPerPage;
+                                if (_offset >= _totalItems) {
+                                    hasNext = false;
+                                } else {
+                                    body.Offset = _offset;
+                                    body.Length = _itemsPerPage;
+                                }
+                                break;
+                            }
+                            default:
+                            {
+                                hasNext = false;
+                                await eventListener.Signal(Microsoft.Rest.ClientRuntime.Events.BeforeResponseDispatch, _response); if( eventListener.Token.IsCancellationRequested ) { return; }
+                                await onDefault(_response,_response.Content.ReadAsStringAsync().ContinueWith( respBody => Nutanix.Powershell.Models.VmStatus.FromJson(Carbon.Json.JsonNode.Parse(respBody.Result)) ));
+                                break;
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        // finally statements
+                        await eventListener.Signal(Microsoft.Rest.ClientRuntime.Events.Finally, request, _response);
+                        _response?.Dispose();
+                        request?.Dispose();
+                    }
+
+                }
+
+                System.Func<System.Threading.Tasks.Task<Nutanix.Powershell.Models.IImageIntentResource[]>> anonfunc = async () => result.ToArray();
+                await onOK(null, anonfunc());
+
+            }
+        }
+
         /// <summary>Actual wire call for <see cref="Vms" /> method.</summary>
         /// <param name="request">the prepared HttpRequestMessage to send.</param>
         /// <param name="onOK">a delegate that is called when the remote service returns 200 (OK).</param>
